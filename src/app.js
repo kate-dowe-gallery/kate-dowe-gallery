54174cacee5927cb80e4c109e87a326dadb28b02
Harry Dowe <harry.dowe@gmail.com>
2018-08-06 23:00:59 +0100

    tweaked primary container layout, added parallax

--- src/app.js
+++ src/app.js
@@ -8,3 +8,11 @@ function galleryClick(event)
         inline: "nearest"
     });
 }
+
+document.querySelector('.primary').style.transform = 'translate3d(0px, ' + window.scrollY / 5 + 'px, 0px)';
+
+window.onscroll = function () {
+    if (this.scrollY / 5 < 175) {
+        document.querySelector('.primary').style.transform = 'translate3d(0px, ' + this.scrollY / 5 + 'px, 0px)';
+    }
+};
