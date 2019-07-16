6c5b2b3514a1461f825322269fc8c76e9e27201e
Harry Dowe <harry.dowe@gmail.com>
2019-07-16 20:54:24 +0100

    Fixed js, copying instead of compiling

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,12 @@ const mix = require('laravel-mix');
 // Disables the mix-manifest.json file
 Mix.manifest.refresh = _ => void 0;
 
+mix.webpackConfig({
+    devServer: {
+        contentBase: 'docs/'
+    }
+});
+
 mix.sass('src/app.scss', 'docs/assets')
     .setPublicPath('docs/assets')
-    .js('src/app.js', 'docs/assets');
+    .copy('src/app.js', 'docs/assets/app.js');
